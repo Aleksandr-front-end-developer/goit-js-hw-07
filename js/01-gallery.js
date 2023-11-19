9ea2872bd92303759b0e69a21506eae08a51700b
Oleksandr Danyliuk <oleksandr.danyliuk.88@gmail.com>
2023-11-19 15:28:45 +0200

    do work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galaryWrapper = document.querySelector('.gallery');
+
+function markup(galleryItems) {
+	return galleryItems.map(({ preview, original, description }) => {
+		return `<li class="gallery__item">
+							<a class="gallery__link" href="${original}">
+								<img
+									class="gallery__image"
+									src="${preview}"
+									data-source="${original}"
+									alt="${description}"
+								/>
+							</a>
+						</li>`
+	}).join('');
+}
+
+const markupGallary = markup(galleryItems);
+
+galaryWrapper.innerHTML = markupGallary;
+
+galaryWrapper.addEventListener('click', showModalImage);
+
+
+function showModalImage(event) {
+	event.preventDefault();
+
+	if (event.target.nodeName !== 'IMG') {
+		return
+	}
+
+	const instance = basicLightbox.create(`
+		<img width="1400" height="900" src="${event.target.dataset.source}">
+	`);
+
+	instance.show()
+
+	if (instance.visible()) {
+		galaryWrapper.addEventListener('keydown', (event) => {
+			if (event.code !== 'Escape') {
+				return
+			}
+			instance.close()
+		});
+	} else {
+		galaryWrapper.removeEventListener()
+	}
+
+}
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galaryWrapper = document.querySelector('.gallery');
+
+function markup(galleryItems) {
+	return galleryItems.map(({ preview, original, description }) => {
+		return `<li class="gallery__item">
+							<a class="gallery__link" href="${original}">
+								<img 
+									class="gallery__image"
+									src="${preview}"									
+									alt="${description}"
+								/>
+							</a>
+						</li>`
+	}).join('');
+}
+
+const markupGallary = markup(galleryItems);
+
+galaryWrapper.innerHTML = markupGallary;
+
+var lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'alt',
+	captionDelay: 250,
+});
